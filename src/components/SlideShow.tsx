92b5ef525b26da5399b56308dd1df770d9802184
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-03 19:14:49 +0200

    feat: enable mousewheel control in SlideShow component

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
 import "swiper/css";
 import MainSlide from "./slides/MainSlide";
 import SkillsSlide from "./slides/SkillsSlide";
@@ -10,6 +11,7 @@ const SlideShow: React.FC = () => {
       direction="vertical"
       slidesPerView={1}
       mousewheel={true}
+      modules={[Mousewheel]}
       style={{ width: "100vw", height: "100vh" }}
     >
       <SwiperSlide className="main-slide">
