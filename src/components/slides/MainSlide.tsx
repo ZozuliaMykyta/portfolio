1c12e8767474d0477ec8bc965a3aedacf8f67ead
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-18 16:42:44 +0200

    feat: update MainSlide component to display a greeting message

--- src/components/slides/MainSlide.tsx
+++ src/components/slides/MainSlide.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MainSlide: React.FC = () => {
-  return <div>MainSlide</div>;
+  return (
+    <>
+      <div>
+        <h4>a message from earth</h4>
+        <h1>hello fellow galaxy member</h1>
+        <h4>a message from earth</h4>
+      </div>
+      <h2>
+        i am <span>mykyta</span>
+      </h2>
+    </>
+  );
 };
 
 export default MainSlide;
