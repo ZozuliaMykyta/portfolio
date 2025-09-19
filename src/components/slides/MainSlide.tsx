1906d3124bc4638c1a3aa021dc29d198e271385c
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-19 18:24:37 +0200

    Add initial greeting section to MainSlide

--- src/components/slides/MainSlide.tsx
+++ src/components/slides/MainSlide.tsx
@@ -2,16 +2,28 @@ import React from "react";
 
 const MainSlide: React.FC = () => {
   return (
-    <>
-      <div>
-        <h4>a message from earth</h4>
-        <h1>hello fellow galaxy member</h1>
-        <h4>a message from earth</h4>
+    <div className="container">
+      <div className="mb-24 flex flex-col justify-center">
+        <div className="flex items-center justify-center gap-4">
+          <div className="bar mt-1"></div>
+          <h4 className="text-custom-purple text-[22px] font-death-star self-end">
+            a message from earth
+          </h4>
+        </div>
+        <h1 className="text-[58px] font-death-star text-with-stroke text-transparent purple-shadow">
+          hello fellow galaxy member
+        </h1>
+        <div className="flex items-center justify-center gap-4">
+          <h4 className="text-custom-purple text-[22px] font-death-star">
+            a message from earth
+          </h4>
+          <div className="bar mt-1"></div>
+        </div>
       </div>
-      <h2>
-        i am <span>mykyta</span>
+      <h2 className="text-[58px] font-death-star text-with-stroke text-transparent purple-shadow">
+        i am mykyta
       </h2>
-    </>
+    </div>
   );
 };
 
