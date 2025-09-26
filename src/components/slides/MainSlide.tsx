c2c9006c242fbf1d1b3d037e897d79841966e0e7
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-26 20:35:59 +0200

    fix: adjust text sizes and layout properties for improved visual consistency in MainSlide

--- src/components/slides/MainSlide.tsx
+++ src/components/slides/MainSlide.tsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const MainSlide: React.FC = () => {
   return (
-    <div className="container">
-      <div className="mb-24 flex flex-col justify-center">
-        <div className="flex items-center justify-center gap-4">
+    <div className="container !mt-[126px]">
+      <div className="mb-14 flex flex-col justify-center">
+        <div className="flex items-center justify-between gap-4">
           <div className="bar mt-1"></div>
-          <h4 className="text-custom-purple text-[22px] font-death-star self-end">
+          <h4 className="text-custom-purple text-[18px] font-death-star self-end">
             a message from earth
           </h4>
         </div>
-        <h1 className="text-[58px] font-death-star text-with-stroke text-transparent purple-shadow">
+        <h1 className="text-[48px] font-death-star text-with-stroke text-transparent purple-shadow">
           hello fellow galaxy member
         </h1>
-        <div className="flex items-center justify-center gap-4">
-          <h4 className="text-custom-purple text-[22px] font-death-star">
+        <div className="flex items-center justify-between gap-4">
+          <h4 className="text-custom-purple text-[18px] font-death-star">
             a message from earth
           </h4>
           <div className="bar mt-1"></div>
         </div>
       </div>
-      <h2 className="text-[58px] font-death-star text-with-stroke text-transparent purple-shadow">
+      <h2 className="text-[48px] font-death-star text-with-stroke text-transparent purple-shadow">
         i am mykyta
       </h2>
     </div>
