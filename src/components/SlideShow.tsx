4c8e883264489d84f9dc2ee187074920a0aaaa72
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-27 14:43:36 +0200

    feat: enhance SlideShow with fade effect and background gradients

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Mousewheel } from "swiper/modules";
+import { Mousewheel, EffectFade } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/effect-fade";
 import MainSlide from "./slides/MainSlide";
 import SkillsSlide from "./slides/SkillsSlide";
 
@@ -15,11 +16,12 @@ const SlideShow: React.FC = () => {
         sensitivity: 1,
         releaseOnEdges: true,
       }}
-      modules={[Mousewheel]}
+      modules={[Mousewheel, EffectFade]}
       style={{ width: "100vw", height: "100vh" }}
       allowTouchMove={false}
       simulateTouch={false}
       touchStartPreventDefault={true}
+      effect={"fade"}
       noSwiping={true}
       noSwipingClass="swiper-no-swiping"
     >
@@ -27,7 +29,7 @@ const SlideShow: React.FC = () => {
         className="slide-structure"
         style={{
           background:
-            "url('assets/img/main-background.png') 0 0 / cover no-repeat",
+            "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('assets/img/main-background.png') 0 0 / cover no-repeat",
         }}
       >
         <MainSlide />
@@ -36,7 +38,7 @@ const SlideShow: React.FC = () => {
         className="slide-structure"
         style={{
           background:
-            "url('assets/img/skills-background.png') 0 0 / cover no-repeat",
+            "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('assets/img/skills-background.png') 0 0 / cover no-repeat",
         }}
       >
         <SkillsSlide />
