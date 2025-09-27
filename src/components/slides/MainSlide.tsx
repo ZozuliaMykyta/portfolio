6eef34a4767041ff2b99d2502cd95e3320958787
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-27 14:43:47 +0200

    feat: update MainSlide to include Social component for enhanced layout

--- src/components/slides/MainSlide.tsx
+++ src/components/slides/MainSlide.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Hero from "../Hero";
+import Social from "../social/Social";
 
 const MainSlide: React.FC = () => {
-  return <Hero />;
+  return (
+    <div>
+      <Hero />
+      <Social />
+    </div>
+  );
 };
 
 export default MainSlide;
