5364aaf63b15aca46dd3987073a1f3c25671ef07
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-28 12:43:08 +0200

    feat: update App component to include EnterIndicator within a wrapper div

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import SlideShow from "./components/SlideShow";
 import "./App.css";
+import EnterIndicator from "./components/EnterIndicator";
 
 function App() {
-  return <SlideShow />;
+  return (
+    <div className="relative">
+      <SlideShow />
+      <EnterIndicator />
+    </div>
+  );
 }
 
 export default App;
