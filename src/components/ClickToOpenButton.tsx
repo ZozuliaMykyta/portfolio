ee40bfb69bbd78279b6ec42b065a63cf879c495a
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-09-30 08:27:00 +0200

    feat: enhance ClickToOpenButton with motion effects for improved interactivity

--- src/components/ClickToOpenButton.tsx
+++ src/components/ClickToOpenButton.tsx
@@ -1,26 +1,89 @@
 import React from "react";
 import Arrow from "../assets/img/arrow.svg";
+import { motion } from "motion/react";
 
 const ClickToOpenButton: React.FC = () => {
   return (
-    <div className="absolute left-[50%] -translate-x-[50%] bottom-[180px] z-50">
-      <div className="relative">
-        <img
+    <motion.div
+      className="absolute left-[50%] -translate-x-[50%] bottom-[180px] z-50 cursor-pointer"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 0.5 }}
+    >
+      <motion.div
+        className="relative"
+        animate={{
+          scale: [1, 1.05, 1],
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      >
+        <motion.img
           src={Arrow}
           alt="Arrow Icon"
           className="absolute top-[-24px] left-[-32px] purple-shadow"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            x: [0, -8, 0],
+            y: [0, -8, 0],
+          }}
+          transition={{
+            opacity: { duration: 0.8, delay: 1 },
+            x: {
+              duration: 1.5,
+              repeat: Infinity,
+              ease: "easeInOut",
+              delay: 1.5,
+            },
+            y: {
+              duration: 1.5,
+              repeat: Infinity,
+              ease: "easeInOut",
+              delay: 1.5,
+            },
+          }}
         />
-        <h3 className="text-center text-[39px] font-death-star white-stroke purple-shadow text-transparent">
+        <motion.h3
+          className="text-center text-[39px] font-death-star white-stroke purple-shadow text-transparent"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8, delay: 0.8 }}
+        >
           Click
           <br /> to Open
-        </h3>
-        <img
+        </motion.h3>
+        <motion.img
           src={Arrow}
           alt="Arrow Icon"
-          className="absolute bottom-[-36px] right-[-48px] rotate-180 purple-shadow"
+          className="absolute bottom-[-36px] right-[-48px] purple-shadow rotate-180"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            x: [0, -8, 0],
+            y: [0, -8, 0],
+          }}
+          transition={{
+            opacity: { duration: 0.8, delay: 1 },
+            x: {
+              duration: 1.5,
+              repeat: Infinity,
+              ease: "easeInOut",
+              delay: 1.5,
+            },
+            y: {
+              duration: 1.5,
+              repeat: Infinity,
+              ease: "easeInOut",
+              delay: 1.5,
+            },
+          }}
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
