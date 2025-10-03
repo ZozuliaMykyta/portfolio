308afc9bbfa70c847aa2f2b93733d46175d6748d
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-03 16:18:22 +0200

    feat: add earth image and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import EnterIndicator from "./components/EnterIndicator";
 import ScrollIndicator from "./components/ScrollIndicator";
 import ContactButton from "./components/contact/ContactButton";
 import ClickToOpenButton from "./components/ClickToOpenButton";
+import earth from "./assets/img/earth.png";
 
 function App() {
   return (
@@ -13,6 +14,11 @@ function App() {
       <ScrollIndicator />
       <ContactButton />
       <ClickToOpenButton />
+      <img
+        className="absolute bottom-[-100%] left-[50%] -translate-x-[50%] z-30"
+        src={earth}
+        alt="Earth"
+      />
     </div>
   );
 }
