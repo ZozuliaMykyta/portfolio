d1a374e14c0e6767a5cfe575553166f4dbb2d02c
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-13 21:47:28 +0200

    feat: enhance SkillsHero component layout and styling for better skills presentation

--- src/components/skills/SkillsHero.tsx
+++ src/components/skills/SkillsHero.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SkillsHero: React.FC = () => {
-  return <h2>my skills</h2>;
+  return (
+    <div className="container flex flex-col items-center justify-center !mt-[150px]">
+      <div className="bar mb-[-10px]"></div>
+      <h4 className="text-[58px] uppercase font-death-star pink-stroke text-transparent purple-shadow text-center">
+        my skills
+      </h4>
+      <div className="bar"></div>
+    </div>
+  );
 };
 
 export default SkillsHero;
