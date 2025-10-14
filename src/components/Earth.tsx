6b3821d728be15aea214ca7d35eaf44577944210
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-14 09:15:23 +0200

    feat: add Earth component with rotating animation

---
+++ src/components/Earth.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import earth from "../assets/img/earth.png";
+import { motion } from "motion/react";
+
+const Earth: React.FC = () => {
+  return (
+    <motion.img
+      className="absolute bottom-0 left-[50%] -translate-x-[50%] translate-y-[66%] z-30 pointer-events-none w-[1100px] h-[1100px]"
+      src={earth}
+      alt="Earth"
+      animate={{ rotate: 360 }}
+      transition={{
+        duration: 100,
+        repeat: Infinity,
+        ease: "linear",
+      }}
+    />
+  );
+};
+
+export default Earth;
