c8a50c4a1462e23fd2b2f87d9f1ced926ec097cf
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2025-10-14 09:15:49 +0200

    feat: add Info component to MainSlide for enhanced content display

--- src/components/slides/MainSlide.tsx
+++ src/components/slides/MainSlide.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Hero";
 import Social from "../social/Social";
+import Info from "../info/Info";
 
 const MainSlide: React.FC = () => {
   return (
     <div>
       <Hero />
       <Social />
+      <Info />
     </div>
   );
 };
